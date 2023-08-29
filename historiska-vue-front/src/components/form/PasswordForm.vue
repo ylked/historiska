7849b8fca71ed9d52acf852de3b02dde9d5d65f1
Miranda Fleury <miranda.fleury@he-arc.ch>
2023-08-29 10:55:51 +0200

    Fix update password form with condition forget to display old password field

--- historiska-vue-front/src/components/form/PasswordForm.vue
+++ historiska-vue-front/src/components/form/PasswordForm.vue
@@ -2,36 +2,74 @@
 import {Form, Field, ErrorMessage} from "vee-validate";
 import * as yup from "yup";
 
+const props = defineProps({
+    forget: {
+        type: Boolean,
+        default: false
+    },
+});
+
 const requiredMessage: string = "Veuillez remplir ce champ";
 const schema = yup.object({
-    password: yup.string()
+    newPassword: yup.string()
         .required(requiredMessage)
         .matches(
             /^(?=.*[A-Za-z])(?=.*\d)(?=.{8,})/,
             "Doit contenir 8 caracters, une majuscule ou une minuscule et un nombre"
         ),
+        //.notOneOf([yup.ref('passwordOld')], 'Le mot passe doit être différent de votre ancien'),
     passwordConfirmation: yup.string()
         .required(requiredMessage)
-        .oneOf([yup.ref('password')], 'Les mots de passe doivent être identiques')
+        .oneOf([yup.ref('newPassword')!], 'Les mots de passe doivent être identiques')
+});
+
+const schemaWithForget = yup.object({
+    passwordOld: yup.string()
+        .required(requiredMessage)
+        .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.{8,})/,
+            "Doit contenir 8 caracters, une majuscule ou une minuscule et un nombre"
+        ),
+    newPassword: yup.string()
+        .required(requiredMessage)
+        .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.{8,})/,
+            "Doit contenir 8 caracters, une majuscule ou une minuscule et un nombre"
+        )
+        .notOneOf([yup.ref('passwordOld')], 'Le mot passe doit être différent de votre ancien'),
+    passwordConfirmation: yup.string()
+        .required(requiredMessage)
+        .oneOf([yup.ref('newPassword')!], 'Les mots de passe doivent être identiques')
 });
 
 function submit(values) {
     // TODO SEND TO API
+
+    if(props.forget) {
+        delete values.passwordOld;
+    }
+
     delete values.passwordConfirmation;
     console.log(JSON.stringify(values, null, 2));
 }
+
 </script>
 
 <template>
-    <Form @submit="submit" class="frm-modal" :validation-schema="schema" v-slot="{ errors }">
+    <Form @submit="submit" class="frm-modal" :validation-schema="forget ? schemaWithForget : schema" v-slot="{ errors }">
         <ul class="frm-items">
-            <li class="frm-item">
-                <Field name="password" type="password" :placeholder="'********'"
-                       :class="{ 'frm-error-field': errors['password'] }" />
-                <ErrorMessage name="password" class="frm-error-message" />
+            <li v-if="forget" class="frm-item">
+                <Field name="passwordOld" type="password" :placeholder="'Ancien mot de passe'"
+                       :class="{ 'frm-error-field': errors['passwordOld'] }" />
+                <ErrorMessage name="passwordOld" class="frm-error-message" />
             </li>
             <li class="frm-item">
-                <Field name="passwordConfirmation" type="password" :placeholder="'********'"
+                <Field name="newPassword" type="password" :placeholder="'Nouveau mot de passe'"
+                       :class="{ 'frm-error-field': errors['newPassword'] }" />
+                <ErrorMessage name="newPassword" class="frm-error-message" />
+            </li>
+            <li class="frm-item">
+                <Field name="passwordConfirmation" type="password" :placeholder="'Confirmation mot de passe'"
                        :class="{ 'frm-error-field': errors['passwordConfirmation'] }" />
                 <ErrorMessage name="passwordConfirmation" class="frm-error-message" />
             </li>
