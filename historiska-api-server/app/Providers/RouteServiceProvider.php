5f1a7fbdc73028d437ea6ad56b15fa1848698ca4
Dekhli Nima <nima.dekhli@he-arc.ch>
2023-09-01 11:11:21 +0200

    delete useless files

--- historiska-api-server/app/Providers/RouteServiceProvider.php
+++ historiska-api-server/app/Providers/RouteServiceProvider.php
@@ -33,8 +33,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
         });
     }
 }
