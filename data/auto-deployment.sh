d37da781ee285e45c9cad1fb9bac88d96e09762f
Dekhli Nima <nima.dekhli@he-arc.ch>
2023-09-01 13:46:26 +0200

    add some sleeps in auto-deployment

--- data/auto-deployment.sh
+++ data/auto-deployment.sh
@@ -55,15 +55,21 @@ EOF
 
 fi
 
+sleep 1
+
 docker exec $container_name sh -c "php artisan key:generate" || (echo "----ERROR--- could not setup application key" && exit 7)
 
+sleep 1
+
 docker exec $container_name sh -c "php artisan migrate --force" || (echo "---ERROR--- could not create database" && exit 8)
+
+sleep 1
 
 # create default admin account with token='0'
 docker exec historiska-database sh -c "mariadb -u root -padmin -h db -D historiska -e \"insert into user (is_admin, username, email, password, is_activated, token, token_issued_at) values (1, 'admin', 'null', '$2a$12$cLpI7PAtHjtDpaBtdzULAuJAhLyFpjzcb8oZX2riQ5EFa3GvepXzG', 1, '0', now()); \""
 
 export HISTORISKA_API_KEY=0
-
+sleep 5
 cd ../data
 python3 generate.py || (echo "---ERROR--- An error occurred while running python script. This is likely due to the server not having finished setting up. Please wait a few minutes and run this script again later." && exit 9)
 
