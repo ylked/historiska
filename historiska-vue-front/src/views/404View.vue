0147b427e5b3dd0a6de6b75edeabc68d427ffcb8
Plumey Simon <simon.plumey@he-arc.ch>
2023-09-01 15:41:02 +0200

    404

--- historiska-vue-front/src/views/404View.vue
+++ historiska-vue-front/src/views/404View.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import Nav from "../components/Nav.vue";
 </script>
 
 <template>
-    <div class="container">
-        <h1>404 not found</h1>
+    <div>
+        <Nav />
+        <div class="content container">
+            <h1>404 not found</h1>
+            <p>Il semblerait que vous vous soyez perdu dans l'histoire...</p>
+        </div>
     </div>
 </template> 
 
 <style scoped lang="scss">
+.content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    height: 80vh;
+
+    h1 {
+        font-size: 6em;
+    }
+}
 </style>
