6e7aae695486813f3f81ce18fdfc04d589dbb361
Plumey Simon <simon.plumey@he-arc.ch>
2023-09-04 14:21:46 +0200

    Nav responsive

--- historiska-vue-front/src/components/Nav.vue
+++ historiska-vue-front/src/components/Nav.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import {useUserStore} from "../stores/useUserStore.ts";
-import {onMounted} from "vue";
+import { useUserStore } from "../stores/useUserStore.ts";
+import { onMounted, ref } from "vue";
 
 const user = useUserStore();
+
+let collapsedNav = ref<boolean>(false);
+
+const toggleNav = () => {
+    collapsedNav.value = !collapsedNav.value;
+    document.documentElement.style.overflow = collapsedNav.value ? 'hidden' : 'auto';
+}
 
 onMounted(async () => {
     await user.getToken;
@@ -19,12 +26,12 @@ onMounted(async () => {
             Consultez votre e-mail
             pour le code d'activation et cliquez sur le lien correspondant pour activer votre compte. <br>
             Vous pouvez également utiliser le lien ci-dessous :
-            <RouterLink :to="{name: 'compte-activation'}">Activation du compte.</RouterLink>
+            <RouterLink :to="{ name: 'compte-activation' }">Activation du compte.</RouterLink>
         </p>
     </div>
 
     <header>
-        <div class="container">
+        <div class="container" :class="{ 'collapsed': collapsedNav }">
             <nav>
                 <RouterLink :to="{ name: 'Accueil' }" class="historika-text">Historiska</RouterLink>
                 <div class="main-nav">
@@ -34,6 +41,17 @@ onMounted(async () => {
                     <RouterLink v-if="user.getToken" :to="{ name: 'Compte' }" class="nav-btn">Compte</RouterLink>
                     <RouterLink v-if="!user.getToken" :to="{ name: 'Connexion' }" class="nav-btn">Connexion</RouterLink>
                     <RouterLink v-if="user.getToken" :to="{ name: 'Deconnexion' }" class="nav-btn">Déconnexion</RouterLink>
+                    <button class="collapsedBtn" @click="toggleNav"><svg v-if="collapsedNav"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="30px" height="30px">
+                            <path
+                                d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z" />
+                        </svg>
+                        <svg v-else="collapsedNav" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="30px"
+                            height="30px">
+                            <path
+                                d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z" />
+                        </svg>
+                    </button>
                 </div>
             </nav>
         </div>
@@ -45,6 +63,7 @@ nav {
     height: 10vh;
     display: flex;
     justify-content: space-between;
+
     .historika-text {
         font-family: $title-font;
         font-size: 2em;
@@ -57,6 +76,10 @@ nav {
         display: flex;
         align-items: center;
 
+        button {
+            display: none;
+        }
+
         .nav-btn {
             padding: 5px 10px;
             color: black;
@@ -68,14 +91,73 @@ nav {
     }
 }
 
+.collapsedBtn {
+    padding: 5px;
+    background: none;
+    border: none;
+}
+
 .info {
     background-color: $beige;
     padding: 15px;
     display: flex;
     align-items: center;
     justify-content: space-around;
+
     p {
         margin: 0;
     }
+}
+
+@media (max-width: 768px) {
+    nav {
+        .main-nav {
+            .nav-btn {
+                display: none;
+            }
+
+            button {
+
+                display: inline-block;
+            }
+        }
+    }
+
+    .container.collapsed {
+        position: absolute;
+        padding: 0;
+        z-index: 9;
+
+        nav {
+            padding-top: 50px;
+            height: 100vh;
+            width: 100vw;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-start;
+            overflow: hidden;
+            background-color: white;
+            z-index: 999;
+
+            .main-nav {
+                margin-top: 75px;
+                display: grid;
+                z-index: 999;
+
+                .nav-btn {
+                    text-align: center;
+                    display: block;
+                }
+
+                button {
+                    margin-top: 30px;
+                }
+            }
+        }
+    }
+
+
+
 }
 </style>
